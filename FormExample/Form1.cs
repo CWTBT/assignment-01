2c917f894cac5e4d23cccc6b86d3de7f39dafe17
CWTBT <lw03232@gmail.com>
2017-01-22 23:49:24 -0600

    First Commit

---
+++ FormExample/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FormExample
+{
+    public partial class Form1 : Form
+    {
+
+        int x;
+        int y;
+        int cellSize;
+        int margin=10;
+        int row = -1;
+        int col = -1;
+        TTTLog.TTT board = new TTTLog.TTT();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+            UpdateSize();
+        }
+
+        private void UpdateSize()
+        {
+            cellSize = (Math.Min(ClientSize.Width, ClientSize.Height) - 2 * margin)/3;
+            if(ClientSize.Width> ClientSize.Height)
+            {
+                x = (ClientSize.Width - 3 * cellSize) / 2;
+                y = margin;
+            }
+            else
+            {
+                x = margin;
+                y= (ClientSize.Height - 3 * cellSize) / 2;
+            }
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+
+            base.OnResize(e);
+            UpdateSize();
+            Refresh();
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            col = (int)Math.Floor((e.X - x)*1.0 / cellSize);
+            row = (int)Math.Floor((e.Y - y)*1.0 / cellSize);
+            board.Place(row, col);
+
+            Refresh();
+            //base.OnMouseDown(e);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+
+            //base.OnPaint(e);
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+
+                    StringFormat stringFormat = new StringFormat();
+                    stringFormat.Alignment = StringAlignment.Center;
+                    stringFormat.LineAlignment = StringAlignment.Center;
+                    System.Drawing.Font font = new System.Drawing.Font("Ubuntu", cellSize * 3 * 72 / 96 / 4);
+                    System.Drawing.Font font2 = new System.Drawing.Font("Ubuntu", cellSize * 3 * 96);
+                    Rectangle rect = new Rectangle(x + i * cellSize, y + j * cellSize, cellSize, cellSize);
+                    Rectangle winbox = new Rectangle(ClientSize.Width, ClientSize.Height, ClientSize.Width/2, ClientSize.Height/2 );
+                    if (board.playing == false)
+                    {
+                        e.Graphics.DrawString("PLAYER" + (board.winner + 1), font, Brushes.DeepPink, x-cellSize/3, y);
+                        e.Graphics.DrawString("WINS", font, Brushes.DeepPink, x - cellSize / 3, y+cellSize);
+                    }
+                    if (board.grid[j, i] == 0) e.Graphics.DrawString("X", font, Brushes.Black, x + i * cellSize, y + j * cellSize);
+                    if (board.grid[j, i] == 1) e.Graphics.DrawString("O", font, Brushes.Black, x + i * cellSize, y + j * cellSize);
+                    e.Graphics.DrawRectangle(Pens.Chocolate,rect);
+                }
+        }
+
+    }
+    
+}
---
+++ FormExample/TTT.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTTLog
+{
+    class TTT
+    {
+        public int[,] grid;
+        public int turn;
+        public bool playing;
+        public int winner;
+        public int playnum;
+
+        public TTT()
+        {
+            turn = 0;
+            grid = new int[3,3];
+            playing = true;
+            winner = -1;
+            for (int i=0;i<grid.GetLength(0);i++)
+            {
+                for (int j=0;j<grid.GetLength(1);j++)
+                {
+                    grid[i, j] = -1;
+                }
+            }
+        }
+
+        public void Place(int h, int w)
+        {
+            if (w < 0 || w > 2 || h < 0 || h > 2) return;
+
+            if (playing == false || playnum == 9)
+            {
+                playing = true;
+                winner = -1;
+                playnum = 0;
+                for (int i = 0; i < grid.GetLength(0); i++)
+                {
+                    for (int j = 0; j < grid.GetLength(1); j++)
+                    {
+                        grid[i, j] = -1;
+                    }
+                }
+            }
+
+            if (grid[h, w] == -1)
+            {
+
+                grid[h, w] = turn;
+                turn = 1 - turn;
+                playnum += 1;
+            }
+
+
+
+            WinCheck(h, w);
+        }
+
+        public void WinCheck(int h, int w)
+        {
+            if (grid[h,0] == grid[h,1] && grid[h,1] == grid[h,2]) //checks row
+            {
+                playing = false;
+                winner = grid[h, 0];
+            }
+
+            if (grid[0, w] == grid[1, w] && grid[1, w] == grid[2, w]) //checks column
+            {
+                playing = false;
+                winner = grid[0, w];
+
+            }
+
+            if (w == h || Math.Abs(w-h) == 2) //All spots that could count for a diagonal win have coordinates of (n,n), (0,2), or (2,0)
+            {
+                if (grid[0,0] == grid[1,1] && grid[1,1] == grid[2,2] && grid[0,0] != -1)
+                {
+                    playing = false;
+                    winner = grid[0, 0];
+                    
+                }
+
+                if (grid [2,0] == grid [1,1] && grid[1,1] == grid[0,2] && grid[2,0] != -1)
+                {
+                    playing = false;
+                    winner = grid[2, 0];
+                    
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string output = "";
+            int h = grid.GetLength(0);
+            int w = grid.GetLength(1);
+            for (int i=0;i<h;i++)
+            {
+                for (int j=0;j<w;j++)
+                {
+                    output += grid[i,j]+" ";
+
+                }
+                output += "\n";
+            }
+            return output;
+        }
+    }
+
+}
